932885b2dce3e8d9deeb79a53613221b69fcdab9
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-08 14:37:09 +0900

    [add] 게시물 1개조회

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -7,9 +7,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -22,6 +26,17 @@ public class ArticleController {
     @GetMapping("/new")
     public String newArticleForm() {
         return "articles/new";
+    }
+
+    @GetMapping("/{id}")
+    public String findSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = repository.findById(id);
+        if (!optArticle.isEmpty()) {
+            model.addAttribute("article", optArticle.get());
+            return "articles/show";
+        } else {
+            return "articles/error";
+        }
     }
 
     @PostMapping("/posts")
